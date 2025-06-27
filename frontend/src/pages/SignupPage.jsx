07c5f376d011d6a418491a198fae61cf25a894c0
Sumit <s7477381@gmail.com>
2025-06-27 18:55:45 +0530

    Name change

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -37,7 +37,7 @@ function SignupPage() {
           <div className="mb-4 flex items-center justify-start gap-2">
             <ShipWheelIcon className="size-9 text-primary" />
             <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
-              Streamify
+          VADO
             </span>
           </div>
           {/* Error message */}
