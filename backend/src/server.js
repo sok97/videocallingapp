a484adc2a565c2b2d2a4d374e550d05b257b84f2
Sumit <s7477381@gmail.com>
2025-06-27 19:21:12 +0530

    my very last commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,6 +30,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/chat', chatRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
--- frontend/src/libs/axios.js
+++ frontend/src/libs/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const BASE_URL  = import.meta.env.MODE === 'development' ? 'http://localhost:5000/api' : '/api';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: BASE_URL,
   withCredentials: true, // ✅ Needed for cookies
 });
 
